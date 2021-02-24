c4257828fb5731a09675a53f04b90d553b4c1535
Reece Walter <reece@reecewalter.com>
2021-02-24 15:55:34 -0500

    adding files

---
+++ js/main.js
@@ -0,0 +1,7 @@
+function hover(element) {
+    element.setAttribute('src', '/Images/transp.png');
+  }
+  
+  function unhover(element) {
+    element.setAttribute('src', '/Images/Reece Headshot 2020.jpg');
+  }
